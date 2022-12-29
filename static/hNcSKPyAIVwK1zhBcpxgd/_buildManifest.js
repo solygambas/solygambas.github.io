a76aa489f26a06f16e6fa1febfe903481c13a62f
solygambas <51904909+solygambas@users.noreply.github.com>
2022-12-29 07:45:36 +0000

    Deploying to gh-pages from @ solygambas/solygambas.github.io@8430eb472fa7964fe03b59c7c79bb99f78508897 🚀

---
+++ static/hNcSKPyAIVwK1zhBcpxgd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1beb14451460885a.css","static/chunks/pages/index-fbc6d2b68a6ef576.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
