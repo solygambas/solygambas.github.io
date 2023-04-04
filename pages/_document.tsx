44c332ac161f4b71e91f43a92420a9814162a74e
solygambas <51904909+solygambas@users.noreply.github.com>
2023-04-04 08:15:22 +0200

    feat: add i18n

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import i18nextConfig from "../next-i18next.config";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    const currentLocale =
+      this.props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale;
+    return (
+      <Html lang={currentLocale as string}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
