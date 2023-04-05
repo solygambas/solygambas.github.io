765c92dc93eaddd2a561832fe5bf773b319d59e3
solygambas <51904909+solygambas@users.noreply.github.com>
2023-04-05 08:20:41 +0200

    feat: add Spanish version

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -7,7 +7,7 @@ module.exports = {
   debug: process.env.NODE_ENV === "development",
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "fr"],
+    locales: ["en", "es", "fr"],
   },
   // localePath,
   reloadOnPrerender: process.env.NODE_ENV === "development",
