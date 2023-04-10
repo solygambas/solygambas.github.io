0f0268bb387f5ee0cd23f22ecaa34769b16723d8
solygambas <51904909+solygambas@users.noreply.github.com>
2023-04-10 15:45:09 +0200

    feat: add Italian version

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -7,7 +7,7 @@ module.exports = {
   debug: process.env.NODE_ENV === "development",
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "es", "fr"],
+    locales: ["en", "es", "fr", "it"],
   },
   // localePath,
   reloadOnPrerender: process.env.NODE_ENV === "development",
