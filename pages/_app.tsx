3916b567681813113f2ad2be12041496db3292e4
solygambas <51904909+solygambas@users.noreply.github.com>
2024-03-08 12:04:01 +0100

    feat: add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-7QK8X84PDZ" />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp);
