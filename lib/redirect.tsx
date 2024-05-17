224c41e694b5163d0233dd76c203cfd3feb0026c
solygambas <51904909+solygambas@users.noreply.github.com>
2024-05-17 13:15:29 +0200

    refactor: convert scripts to ts

--- lib/redirect.tsx
+++ lib/redirect.tsx
@@ -2,21 +2,23 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import languageDetector from "./languageDetector";
 
-export const useRedirect = (to) => {
+export const useRedirect = (to?: string) => {
   const router = useRouter();
   to = to || router.asPath;
 
   // Language detection
   useEffect(() => {
     const detectedLng = languageDetector.detect();
-    if (to.startsWith("/" + detectedLng) && router.route === "/404") {
-      // Prevent endless loop
-      router.replace("/" + detectedLng + router.route);
-      return;
-    }
+    if (detectedLng) {
+      if (to.startsWith("/" + detectedLng) && router.route === "/404") {
+        // Prevent endless loop
+        router.replace("/" + detectedLng + router.route);
+        return;
+      }
 
-    languageDetector.cache(detectedLng);
-    router.replace("/" + detectedLng + to);
+      languageDetector.cache && languageDetector.cache(detectedLng);
+      router.replace("/" + detectedLng + to);
+    }
   });
 
   return <></>;
@@ -28,7 +30,7 @@ export const Redirect = () => {
 };
 
 // eslint-disable-next-line react/display-name
-export const getRedirect = (to) => () => {
+export const getRedirect = (to: string) => () => {
   useRedirect(to);
   return <></>;
 };
