fc0c659682353c4d6e50ed24edd56891419e42e5
solygambas <51904909+solygambas@users.noreply.github.com>
2024-05-17 15:02:45 +0200

    fix: disambiguation

--- pages/[locale]/case-studies/ho36-hostels.tsx
+++ pages/[locale]/case-studies/ho36-hostels.tsx
@@ -6,7 +6,7 @@ function HoThirtySixHostels() {
   const paragraphs = [
     "Over the past decade, I've specialized in WordPress projects, often collaborating with first-time clients to align their goals with their budget constraints.",
     "One notable project was Ho36 Hostels, where I contributed to their website redesign. While WordPress allows autonomous users to achieve much independently, my role was to address lingering issues and add professional touches that enhance visitor experience.",
-    "Another crucial aspect was integrating the Mews booking system. This integration streamlined the reservation process, allowing independent accommodations to avoid hefty commissions from third-party booking platforms.",
+    "Another crucial aspect was integrating the Mews booking system. This integration streamlined the reservation process, allowing Ho36 Hostels, as an independent accommodation group, to avoid hefty commissions from third-party booking platforms.",
     "Collaborating closely with the marketing team, I gained insights into their business objectives and successfully implemented solutions within tight deadlines.",
   ];
 
