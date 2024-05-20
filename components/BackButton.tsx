0987d89d37abc5b417b7b9308b4c448af8b4f23e
solygambas <51904909+solygambas@users.noreply.github.com>
2024-05-20 11:14:26 +0200

    chore: add i18n to back button

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,11 +1,13 @@
 import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
 import styles from "../styles/BackButton.module.css";
 
 function BackButton() {
   const router = useRouter();
+  const { t } = useTranslation("common");
   return (
     <button className={styles.button} onClick={() => router.back()}>
-      Back
+      {t("backButton.text")}
     </button>
   );
 }
