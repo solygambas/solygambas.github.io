167bfb42d8a881ce5c333167ca5e7886b168bcf1
solygambas <51904909+solygambas@users.noreply.github.com>
2024-05-24 15:04:14 +0200

    fix: html lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,26 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
 import i18nextConfig from "../next-i18next.config";
 
-class MyDocument extends Document {
+interface DocumentProps extends DocumentInitialProps {
+  currentLocale: string;
+}
+
+class MyDocument extends Document<DocumentProps> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const currentLocale = ctx.query.locale || i18nextConfig.i18n.defaultLocale;
+    return { ...initialProps, currentLocale };
+  }
+
   render() {
-    const currentLocale =
-      this.props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale;
+    const { currentLocale } = this.props;
     return (
       <Html lang={currentLocale as string}>
         <Head />
